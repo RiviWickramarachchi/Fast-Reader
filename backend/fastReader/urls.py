b1a7e98e9f113895040cb11b019479dd6773c8e4
Rivindu Wickramarachchi <riviwicky95@gmail.com>
2024-09-28 20:22:09 +0200

    added methods to get and delete file items via backend

--- backend/fastReader/urls.py
+++ backend/fastReader/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import DocumentUploadView
+from .views import DocumentUploadView, SingleDocumentView
 
 
 # router = routers.DefaultRouter()
 # router.register(r'docs', DocumentUploadView, 'doc')
 
 urlpatterns = [
-   path('documents/', DocumentUploadView.as_view(), name='upload-document')
+   path('documents/', DocumentUploadView.as_view(), name='upload-document'),
+   path('documents/<int:id>/', SingleDocumentView.as_view(), name='document-detail'),
     #path('', include(router.urls)),
 ]
 
--- backend/fastReader/views.py
+++ backend/fastReader/views.py
@@ -21,37 +21,78 @@ class DocumentUploadView(APIView):
         # Deserialize the incoming request data
         file_serializer = DocumentSerializer(data=request.data)
         if file_serializer.is_valid():
-            file_serializer.save()
 
-            # Process the file after saving it
-            file_instance = file_serializer.instance
-            content = self.process_file(file_instance.file)
+             # Get the file from the request data (without saving it to the database)
+            file = request.data.get('file')
+            common_methods = CommonMethods()
+            content, isReadable = common_methods.process_file(file)
 
-            return Response({"message": "File uploaded successfully", "content": content}, status=status.HTTP_201_CREATED)
+            #Save the file if it is in a readable format
+            if(isReadable):
+                file_serializer.save()
+                return Response({"message": "File uploaded successfully", "content": content}, status=status.HTTP_201_CREATED)
+            else:
+                return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def process_file(self, file):
+
+class SingleDocumentView(APIView):
+    def get(self, request, id, *args, **kwargs):
+
+        #GET request to return a single Document object by its ID.
+        try:
+            document = Document.objects.get(pk=id)  # Get the document by ID
+            file = document.file #get the file data from the object
+            common_methods = CommonMethods()
+            data, isReadable = common_methods.process_file(file) #convert the file to readable format
+            return Response( {"data": data} , status=status.HTTP_200_OK)
+        except Document.DoesNotExist:
+            return Response({"error": "Document not found"}, status=status.HTTP_404_NOT_FOUND)
+    def delete(self, request, id=None, *args, **kwargs):
+        try:
+            document = Document.objects.get(pk=id)
+            document.delete()
+            return Response({"message": "Document deleted successfully!"}, status=status.HTTP_204_NO_CONTENT)
+        except Document.DoesNotExist:
+            return Response({"error": "Document not found!"}, status=status.HTTP_404_NOT_FOUND)
+
+class CommonMethods():
+     def process_file(self, file):
         # Determine if the file is Word or PDF and process accordingly
         file_name = file.name.lower()
 
         if file_name.endswith('.docx'):
-            return self.read_docx(file)
+            return self.read_docx(file), True
         elif file_name.endswith('.pdf'):
-            return self.read_pdf(file)
+            return self.read_pdf(file), True
 
-        return "Unsupported file format"
+        return "Unsupported file format", False
+     def read_docx(self, file):
 
-    def read_docx(self, file):
+        single_text_arr = []
         # Extract text from Word (.docx) file
         doc = docx.Document(file)
         full_text = [paragraph.text for paragraph in doc.paragraphs]
-        return "\n".join(full_text)
+        #formatted_text.extend(full_text.splitlines())
+        for element in full_text:
+            single_text_arr.extend(element.split())
+        #print(single_text_arr)
+        return " ".join(full_text)
 
-    def read_pdf(self, file):
+     def read_pdf(self, file):
         # Extract text from PDF file using pdfplumber
         full_text = ""
+        formatted_text = []
+        single_text_arr =[]
         with pdfplumber.open(file) as pdf:
             for page in pdf.pages:
-                full_text += page.extract_text() + "\n"
-        return full_text
+                #full_text += page.extract_text() + "\n"
+                full_text += page.extract_text()
+                formatted_text.extend(full_text.splitlines())
+            for element in formatted_text:
+                single_text_arr.extend(element.split())
+            print(single_text_arr)
+        return formatted_text
+
+     #write a function to support text files
