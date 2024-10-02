b1cc9f9f9fea15f4140daee212de155087de5883
Rivindu Wickramarachchi <riviwicky95@gmail.com>
2024-10-02 23:25:27 +0200

    added methods to frontend so that words can be displayed inside the textbox

--- backend/fastReader/views.py
+++ backend/fastReader/views.py
@@ -23,14 +23,16 @@ class DocumentUploadView(APIView):
         if file_serializer.is_valid():
 
              # Get the file from the request data (without saving it to the database)
+            
             file = request.data.get('file')
             common_methods = CommonMethods()
             content, isReadable = common_methods.process_file(file)
 
             #Save the file if it is in a readable format
             if(isReadable):
-                file_serializer.save()
-                return Response({"message": "File uploaded successfully", "content": content}, status=status.HTTP_201_CREATED)
+                file_instance = file_serializer.save()
+                id = file_instance.id
+                return Response({"message": "File uploaded successfully", "content": id}, status=status.HTTP_201_CREATED)
             else:
                 return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
@@ -45,8 +47,8 @@ class SingleDocumentView(APIView):
             document = Document.objects.get(pk=id)  # Get the document by ID
             file = document.file #get the file data from the object
             common_methods = CommonMethods()
-            data, isReadable = common_methods.process_file(file) #convert the file to readable format
-            return Response( {"data": data} , status=status.HTTP_200_OK)
+            words, isReadable = common_methods.process_file(file) #convert the file to readable format
+            return Response( {"data": words} , status=status.HTTP_200_OK)
         except Document.DoesNotExist:
             return Response({"error": "Document not found"}, status=status.HTTP_404_NOT_FOUND)
     def delete(self, request, id=None, *args, **kwargs):
@@ -78,7 +80,7 @@ class CommonMethods():
         for element in full_text:
             single_text_arr.extend(element.split())
         #print(single_text_arr)
-        return " ".join(full_text)
+        return single_text_arr
 
      def read_pdf(self, file):
         # Extract text from PDF file using pdfplumber
@@ -93,6 +95,6 @@ class CommonMethods():
             for element in formatted_text:
                 single_text_arr.extend(element.split())
             print(single_text_arr)
-        return formatted_text
+        return single_text_arr
 
      #write a function to support text files
--- backend/frontend/src/pages/Home.jsx
+++ backend/frontend/src/pages/Home.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Home = () => {
   const [file, setFile] = useState(null);
   const [title, setTitle] = useState('');
   const [message, setMessage] = useState('');
+  const [docId, setDocId] = useState(null);
+  const [wordList, setWordList] = useState([]);
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const[currWord, setCurrWord] = useState('');
+  const[isStarted,setIsStarted] = useState(false);
 
   // Function to handle file selection
   const handleFileChange = (e) => {
@@ -37,12 +42,63 @@ const Home = () => {
           'Content-Type': 'multipart/form-data',
         },
       });
-
-      setMessage("File uploaded successfully!");
+     console.log(response.data.content);
+     setDocId(response.data.content);
+     setMessage("File uploaded successfully!");
     } catch (error) {
       setMessage("Failed to upload file.");
     }
   };
+
+  //Function to get the list of words in the pdf/doc file and display each word in the textbox
+  const showWords = async (e) => {
+    e.preventDefault();
+
+    try{
+        const response = await axios.get(`http://127.0.0.1:8000/api/documents/${docId}/`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+
+        //start displaying words
+        setWordList(response.data.data);
+        setIsStarted(true);
+
+
+
+        //trigger the bool value to run the useEffect
+        /*console.log(response.data.data);
+        console.log("Word List " +wordList);*/
+
+        //iterate through the arraylist
+        
+    }
+    catch (error) {
+    }
+  };
+
+  useEffect(() => {
+    if (!isStarted) return; // If not started, return
+
+
+    // Function to update the current word at each interval
+    const wordInterval = setInterval(() => {
+      if(currentWordIndex >= wordList.length)
+      {
+        //Stop displaying the words
+        setIsStarted(false);
+        return;
+      }
+      setCurrWord(wordList[currentWordIndex]); // Update the current word
+      setCurrentWordIndex(prevIndex => prevIndex + 1); // Move to the next word
+    }, 1000);
+
+    // Cleanup the interval when the component is unmounted or the interval stops
+    return () => clearInterval(wordInterval);
+  }, [isStarted, currentWordIndex]);
 
   return (
     <div>
@@ -53,8 +109,18 @@ const Home = () => {
         <input type="file" onChange={handleFileChange} />
         <button className="btn" type="submit">Upload</button>
       </form>
+      <br>
+      </br>
       {message && <p>{message}</p>}
+      <div>
+        <br>
+        </br>
+        <button className="btn" onClick={showWords} >Read</button>
+        <h3> Text Area </h3>
+        <textarea id="txtbx" type="text" name="textbx" rows="4" cols="50" value={currWord} readOnly></textarea>
+      </div>
     </div>
+
   );
 };
 
